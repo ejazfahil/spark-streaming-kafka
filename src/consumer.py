04baf4b20c805411bde648e2276520d8ca0c0be0
Fahil Ejaz <ejazfahil@gmail.com>
2022-12-05 11:45:00 +0100

    feat: add Kafka consumer with schema registry

---
+++ src/consumer.py
@@ -0,0 +1,18 @@
+"""Kafka consumer 2022-12-05"""
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col, window
+from pyspark.sql.types import StructType, StringType, DoubleType, TimestampType
+
+SCHEMA = StructType().add("event_id", StringType()).add("user_id", StringType()) \
+    .add("amount", DoubleType()).add("ts", TimestampType())
+
+spark = SparkSession.builder.appName("KafkaConsumer") \
+    .config("spark.jars.packages","org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.0") \
+    .getOrCreate()
+
+df = spark.readStream.format("kafka") \
+    .option("kafka.bootstrap.servers","localhost:9092") \
+    .option("subscribe","payments") \
+    .option("startingOffsets","latest").load()
+
+parsed = df.select(from_json(col("value").cast("string"), SCHEMA).alias("d")).select("d.*")
