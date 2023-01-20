022e26f6f22ed44d89131f3577979348edb5a972
Fahil Ejaz <ejazfahil@gmail.com>
2023-01-20 15:15:00 +0100

    feat: add dead-letter queue handler

---
+++ src/dlq_handler.py
@@ -0,0 +1,13 @@
+"""DLQ handler 2023-01-20"""
+from pyspark.sql.functions import col, current_timestamp
+
+def route_to_dlq(df, error_col="parse_error"):
+    """Send malformed records to dead-letter topic."""
+    bad = df.filter(col(error_col).isNotNull()) \
+           .withColumn("dlq_ts", current_timestamp())
+    return bad.selectExpr(
+        "CAST(event_id AS STRING) AS key",
+        "to_json(struct(*)) AS value"
+    ).writeStream.format("kafka") \
+     .option("kafka.bootstrap.servers","localhost:9092") \
+     .option("topic","payments_dlq").start()
