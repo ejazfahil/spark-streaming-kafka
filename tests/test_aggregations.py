479afa82b7658bac746b069c5eafed8cd89317a2
Fahil Ejaz <ejazfahil@gmail.com>
2023-02-07 16:30:00 +0100

    test: add streaming unit tests with pytest

---
+++ tests/test_aggregations.py
@@ -0,0 +1,17 @@
+"""Streaming tests 2023-02-07"""
+import pytest
+from datetime import datetime
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StringType, DoubleType, TimestampType
+
+@pytest.fixture(scope="session")
+def spark():
+    return SparkSession.builder.master("local[2]").appName("test").getOrCreate()
+
+def test_hourly_totals_schema(spark):
+    schema = StructType().add("user_id",StringType()).add("amount",DoubleType()) \
+                         .add("ts",TimestampType())
+    data = [("u1",10.0,datetime(2023,5,1,10,0)),("u1",20.0,datetime(2023,5,1,10,15))]
+    df = spark.createDataFrame(data, schema)
+    assert df.count() == 2
+    assert "amount" in df.columns
